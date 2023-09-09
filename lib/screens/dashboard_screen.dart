a8ba734de2da8bbd9e01687db23834d2f2d8796a
AbdallahMabrouck <mabrouck32@gmail.com>
2023-09-09 23:15:14 +0300

    modified dashboard

--- lib/screens/dashboard_screen.dart
+++ lib/screens/dashboard_screen.dart
@@ -1,18 +1,19 @@
 import 'package:flutter/material.dart';
 
 class MainScreen extends StatefulWidget {
-  const MainScreen({super.key});
+  const MainScreen({Key? key}) : super(key: key);
 
   @override
-  State<MainScreen> createState() => _MainScreenState();
+  MainScreenState createState() => MainScreenState();
 }
 
-class _MainScreenState extends State<MainScreen> {
+class MainScreenState extends State<MainScreen> {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-        body: Center(
-      child: Text("Dashboard Screen"),
-    ));
+      body: Center(
+        child: Text("Dashboard Screen"),
+      ),
+    );
   }
 }
