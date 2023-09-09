75aef0b89a6ebbc1fe4cb5da1486106bddf90871
AbdallahMabrouck <mabrouck32@gmail.com>
2023-09-09 10:50:03 +0300

    added order provider

--- lib/provider/product_provider.dart
+++ lib/provider/product_provider.dart
@@ -1,9 +1,175 @@
-
-
-
+import 'dart:io';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
+class ProductProvider with ChangeNotifier {
+  String? selectedCategory;
+  String? selectedSubCategory;
+  String? categoryImage;
+  File? image;
+  String pickerError = "";
+  String? shopName;
+  String? productUrl;
+
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+
+  selectCategory(mainCategory, categoryImage) {
+    selectedCategory = mainCategory;
+    this.categoryImage = categoryImage;
+    notifyListeners();
+  }
+
+  selectSubCategory(selected) {
+    selectedSubCategory = selected;
+    notifyListeners();
+  }
+
+  getShopName(shopName) {
+    this.shopName = shopName;
+    notifyListeners();
+  }
+
+  resetProvider() {
+    selectedCategory = null;
+    selectedSubCategory = null;
+    categoryImage = null;
+    image = null;
+    productUrl = null;
+    notifyListeners();
+  }
+
+  Future<String> uploadProductImage(filePath, productName) async {
+    File file = File(filePath);
+    var timeStamp = Timestamp.now().millisecondsSinceEpoch;
+
+    FirebaseStorage _storage = FirebaseStorage.instance;
+
+    try {
+      await _storage
+          .ref("productImage/$shopName/$productName$timeStamp")
+          .putFile(file);
+    } on FirebaseException catch (e) {
+      print(e.code);
+    }
+
+    String downloadURL = await _storage
+        .ref("productImage/$shopName$productName$timeStamp")
+        .getDownloadURL();
+    productUrl = downloadURL;
+    notifyListeners();
+    return downloadURL;
+  }
+
+  Future<File> getProductImage() async {
+    final picker = ImagePicker();
+    final pickedFile = await picker.pickImage(
+      source: ImageSource.gallery,
+      imageQuality: 20,
+    );
+    if (pickedFile != null) {
+      image = File(pickedFile.path);
+      notifyListeners();
+    } else {
+      pickerError = "No image selected";
+      print("No image selected");
+      notifyListeners();
+    }
+    return image!;
+  }
+
+  void alertDialog({title, content}) {
+    showDialog(
+      context: _scaffoldKey.currentContext!,
+      builder: (BuildContext context) {
+        return CupertinoAlertDialog(
+          title: Text(title),
+          content: Text(content),
+          actions: [
+            CupertinoDialogAction(
+              child: const Text("OK"),
+              onPressed: () {
+                Navigator.pop(context);
+              },
+            )
+          ],
+        );
+      },
+    );
+  }
+
+  Future<void> saveProductDataToDb({
+    required String productName,
+    required String description,
+    required double price,
+    required double comparedPrice,
+    required String collection,
+    required String brand,
+    required String sku,
+    required String categoryName,
+    required double weight,
+    required double tax,
+    required int stockQty,
+    required int lowStockQty,
+  }) async {
+    var timeStamp = DateTime.now().millisecondsSinceEpoch;
+    User? user = FirebaseAuth.instance.currentUser;
+    CollectionReference _products =
+        FirebaseFirestore.instance.collection("products");
+
+    try {
+      await _products.doc(timeStamp.toString()).set({
+        "seller": {"shopName": shopName, "sellerUid": user!.uid},
+        "productName": productName,
+        "description": description,
+        "price": price,
+        "comparedPrice": comparedPrice,
+        "collection": collection,
+        "brand": brand,
+        "sku": sku,
+        "category": {
+          "mainCategory": selectedCategory,
+          "subCategory": selectedSubCategory,
+          "categoryImage": categoryImage,
+        },
+        "weight": weight,
+        "tax": tax,
+        "stockQty": stockQty,
+        "lowStockQty": lowStockQty,
+        "published": false,
+        "productId": timeStamp.toString(),
+        "productImage": productUrl,
+      });
+
+      alertDialog(
+        title: "SAVE DATA",
+        content: "Product Details saved successfully",
+      );
+    } catch (e) {
+      alertDialog(
+        title: "SAVE DATA",
+        content: e.toString(),
+      );
+    }
+  }
+}
+
+
+
+
+
+
+/*import 'dart:io';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class ProductProvider with ChangeNotifier{
   String selectedCategory;
@@ -15,7 +181,7 @@ class ProductProvider with ChangeNotifier{
   String productUrl;
 
   selectCategory(mainCategory, categoryImage){
-    this.selectedCategory = mainCategory;
+    selectedCategory = mainCategory;
     this.categoryImage = categoryImage;
     // need to bring image here
     notifyListeners();
@@ -23,7 +189,7 @@ class ProductProvider with ChangeNotifier{
   }
 
   selectSubCategory(selected){
-    this.selectedSubCategory = selected;
+    selectedSubCategory = selected;
     notifyListeners();
 
   }
@@ -35,11 +201,11 @@ class ProductProvider with ChangeNotifier{
 
   resetProvider(){
     // remove all the existing data before update next product
-  this.selectedCategory = null;
-  this.selectedSubCategory = null;
-  this.categoryImage = null;
-  this.image = null;
-  this.productUrl = null;
+  selectedCategory = null;
+  selectedSubCategory = null;
+  categoryImage = null;
+  image = null;
+  productUrl = null;
   notifyListeners();
   }
 
@@ -67,16 +233,16 @@ class ProductProvider with ChangeNotifier{
 
   Future<File>getProductImage() async {
     final picker = ImagePicker();
-    final pickedFile = await picker.getImage(source: ImageSource.gallery, imageQuality:20);
+    final pickedFile = await picker.pickImage(source: ImageSource.gallery, imageQuality:20);
     if(pickedFile != null){
-      this.image = File(pickedFile.path);
+      image = File(pickedFile.path);
       notifyListeners();
     } else {
-      this.pickerError = "No image selected";
+      pickerError = "No image selected";
       print ("No image selected");
       notifyListeners();
     }
-    return this.image;
+    return image;
   }
 
   alertDialog({context, title, content}){
@@ -118,11 +284,11 @@ class ProductProvider with ChangeNotifier{
     
     var timeStamp = DateTime.now().millisecondsSinceEpoch;
     // this will be used as product id
-    User user = FirebaseAuth.instance.currentUser;
+    User? user = FirebaseAuth.instance.currentUser;
     CollectionReference _products = FirebaseFirestore.instance.collection("products");
     try {
       _products.doc(timeStamp.toString()).set({
-      "seller" : {"shopName" : this.shopName, "sellerUid" :user.uid },
+      "seller" : {"shopName" : shopName, "sellerUid" :user!.uid },
       "productName" : productName,
       "description" : description,
       "price" : price, 
@@ -217,7 +383,7 @@ class ProductProvider with ChangeNotifier{
 
 
 
-/*import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 
 class ProductProvider with ChangeNotifier {
