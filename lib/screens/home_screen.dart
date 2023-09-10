d96bb4fd70b25d4fd5eb07d047c1894d42a816d4
AbdallahMabrouck <mabrouck32@gmail.com>
2023-09-10 08:46:07 +0300

    modified home screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,31 +1,122 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import '../provider/vendor_provider.dart';
-import '../widgets/custom_drawer.dart';
+import 'package:flutter_slider_drawer/flutter_slider_drawer.dart';
 
-class HomeScreen extends StatelessWidget {
+import '../widgets/drawer_menu_widget.dart';
+import 'dashboard_screen.dart';
+
+class HomeScreen extends StatefulWidget {
   static const String id = "home-screen";
-  const HomeScreen({super.key});
+
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  final GlobalKey<SliderDrawerState> _sliderDrawerKey =
+      GlobalKey<SliderDrawerState>();
+  late String title = "Default Title";
 
   @override
   Widget build(BuildContext context) {
-    final _vendorData = Provider.of<VendorProvider>(context);
-    if (_vendorData.doc == null) {
-      _vendorData.getVendorData();
-    }
-
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Dashboard"),
-        elevation: 0,
-      ),
-      drawer: const CustomDrawer(),
-      body: const Center(
-        child: Text(
-          "Dashboard",
-          style: TextStyle(fontSize: 22),
+      body: SliderDrawer(
+        appBar: SliderAppBar(
+          appBarHeight: 80,
+          appBarColor: Colors.white,
+          title: const Text(""),
+          trailing: Row(
+            children: [
+              IconButton(
+                  onPressed: () {}, icon: const Icon(CupertinoIcons.search)),
+              IconButton(
+                  onPressed: () {}, icon: const Icon(CupertinoIcons.bell))
+            ],
+          ),
         ),
+        key: _sliderDrawerKey,
+        sliderOpenSize: 250,
+        slider: MenuWidget(
+          onItemClick: (String title) {
+            _sliderDrawerKey.currentState!.closeSlider();
+            setState(() {
+              title = title;
+            });
+          },
+        ),
+        child: const MainScreen(),
       ),
     );
   }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*import 'package:flutter/material.dart';
+import 'package:flutter_slider_drawer/flutter_slider_drawer.dart';
+
+import '../widgets/drawer_menu_widget.dart';
+import 'dashboard_screen.dart';
+
+
+class HomeScreen extends StatefulWidget {
+
+  static const String id = "home-screen"; 
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+
+  // DrawerServices _services == DrawerServices();
+
+  final GlobalKey<SliderDrawerState> _sliderDrawerKey =
+      GlobalKey<SliderDrawerState>();
+  late String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      body: SliderDrawer(
+            appBar: SliderAppBar(
+                appBarColor: Colors.white,
+                title: Text(title,
+                    style: const TextStyle(
+                        fontSize: 22, fontWeight: FontWeight.w700))),
+            key: _sliderDrawerKey,
+            sliderOpenSize: 179,
+            slider: MenuWidget(onItemClick: (title) {
+              _sliderDrawerKey.currentState!.closeSlider();
+              setState(() {
+                this.title = title;
+              });
+            }),
+            child: const MainScreen()),
+    );
+  }
+}*/
