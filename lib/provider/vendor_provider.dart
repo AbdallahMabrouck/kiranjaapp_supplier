e91d09adaf3056ca14db7f0f02b9cb9f4dee1e4f
AbdallahMabrouck <mabrouck32@gmail.com>
2023-09-10 08:49:12 +0300

    created vendor provider

--- lib/provider/vendor_provider.dart
+++ lib/provider/vendor_provider.dart
@@ -1,6 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import '../firebase_services.dart';
+import '../services/firebase_services.dart';
 import '../models/vendor_model.dart';
 
 class VendorProvider with ChangeNotifier {
