b17d78575897e5344f24b0970ff25b292187f747
AbdallahMabrouck <mabrouck32@gmail.com>
2023-09-10 21:44:09 +0300

    modified register screen

--- lib/screens/register_screen.dart
+++ lib/screens/register_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import '../widgets/image_picker.dart';
 import '../widgets/register_form.dart';
-import 'login_screen.dart';
 
 class RegisterScreen extends StatelessWidget {
   const RegisterScreen({super.key});
@@ -10,40 +9,17 @@ class RegisterScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
+    return const SafeArea(
       child: Scaffold(
         body: Center(
           child: Padding(
-            padding: const EdgeInsets.all(20.0),
+            padding: EdgeInsets.all(20.0),
             child: SingleChildScrollView(
               scrollDirection: Axis.vertical,
               child: Column(
                 children: [
-                  const ShopPicCard(),
-                  const RegisterForm(),
-                  Row(
-                    children: [
-                      ElevatedButton(
-                        onPressed: () {
-                          Navigator.pushNamed(context, LoginScreen.id);
-                        },
-                        child: Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: RichText(
-                            text: const TextSpan(text: "", children: [
-                              TextSpan(
-                                  text: "Already have an account ? ",
-                                  style:
-                                      TextStyle(fontWeight: FontWeight.bold)),
-                              TextSpan(
-                                  text: "Login",
-                                  style: TextStyle(fontWeight: FontWeight.bold))
-                            ]),
-                          ),
-                        ),
-                      ),
-                    ],
-                  )
+                  ShopPicCard(),
+                  RegisterForm(),
                 ],
               ),
             ),
